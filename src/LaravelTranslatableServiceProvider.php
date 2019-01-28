f5c0b8346b75fa90a7bacfe5b7f996e73a7211fe
Abid Raza <ari.abid@gmail.com>
2019-01-29 00:02:36 +0530

    Init composer and create service provider

---
+++ src/LaravelTranslatableServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AriAbid\LaravelTranslatable;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTranslatableServiceProvider extends ServiceProvider {
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        # code...
+    }
+}
